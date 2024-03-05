af9fe2d8773d1708e7a0e93d4e476fc23848a1ac
Daniel Vargas Castro <104041838+VargasCastroDaniel@users.noreply.github.com>
2024-03-04 23:06:23 -0600

    feat: CameraCard, props y estado reciclable

--- src/components/CameraCard.js
+++ src/components/CameraCard.js
@@ -1,18 +1,25 @@
 import React from "react";
-import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
+import { Card, CardContent } from "@mui/material";
+import VideocamIcon from "@mui/icons-material/Videocam";
 import "./styles/styleInicio.css";
 
-function CameraCard() {
+function CameraCard({ title, estado, onClick }) {
   return (
     <div className="cameraCard">
-      <Card>
-        <CardMedia
-          className="cameraMedia"
-          component="img"
-          image="URL_DE_LA_IMAGEN"
-          title="Título de la cámara"
-        />
+      <Card onClick={onClick}>
+        <CardContent className="cameraContent">
+          <div className="title">{title}</div>
+          <div className="iconContainer">
+            <VideocamIcon fontSize="inherit" />
+          </div>
+          <span
+            className={`statusText${
+              estado === "Offline" ? " Offline" : " Online"
+            }`}
+          >
+            {estado}
+          </span>
+        </CardContent>
       </Card>
     </div>
   );
