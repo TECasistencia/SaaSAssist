0a537ea2cc7c35c9c89c28854fcffef3fd3115d8
Daniel Vargas Castro <104041838+VargasCastroDaniel@users.noreply.github.com>
2024-03-04 23:07:55 -0600

    style: box en header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography, Box } from "@mui/material";
+import CustomMenu from "./CustomMenu";
 
 function Header() {
   return (
     <AppBar position="static" className="header">
       <Toolbar>
         <Typography variant="h6">Mi Aplicación</Typography>
+        <Box sx={{ flexGrow: 1 }} />
+        <CustomMenu />
       </Toolbar>
     </AppBar>
   );
