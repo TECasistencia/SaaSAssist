e3c2a2b912085d1221c0793d9965270bccde87db
Daniel Vargas Castro <104041838+VargasCastroDaniel@users.noreply.github.com>
2024-03-04 23:10:02 -0600

    fix: PageInicio
    map de camara, dialog de startcamera

--- src/components/PageInicio.js
+++ src/components/PageInicio.js
@@ -1,30 +1,86 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
-import Menu from "./Menu";
 import Footer from "./Footer";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import CameraCard from "./CameraCard";
+import StartCamera from "../modals/StartCamera";
+import { Dialog, DialogContent } from "@mui/material";
+
+const initialCameras = [
+  { title: "Título de la cámara 1", estado: "Offline" },
+  { title: "Título de la cámara 2", estado: "Offline" },
+  { title: "Título de la cámara 3", estado: "Offline" },
+];
 
 function PageInicio() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedCameraTitle, setSelectedCameraTitle] = useState("");
+  const [cameras, setCameras] = useState(initialCameras);
+
+  const handleCardClick = (title) => {
+    setSelectedCameraTitle(title);
+    setIsModalOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleStart = () => {
+    // Cambia el estado de la cámara seleccionada a "online"
+    const updatedCameras = cameras.map((camera) => ({
+      ...camera,
+      estado: camera.title === selectedCameraTitle ? "Online" : "Offline",
+    }));
+
+    // Si hay otra cámara en línea, la pone en línea automáticamente
+    const onlineCamerasCount = updatedCameras.filter(
+      (camera) => camera.estado === "Online"
+    ).length;
+    if (onlineCamerasCount > 1) {
+      updatedCameras.forEach((camera) => {
+        if (
+          camera.title !== selectedCameraTitle &&
+          camera.estado === "Online"
+        ) {
+          camera.estado = " Offline";
+        }
+      });
+    }
+
+    setCameras(updatedCameras);
+    setIsModalOpen(false);
+  };
+
   return (
     <div>
       <Header />
-      <Menu />
       <Box sx={{ flexGrow: 1 }} className="box">
         <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          <Grid item xs={2}>
-            <CameraCard />
-          </Grid>
-          <Grid item xs={2}>
-            <CameraCard />
-          </Grid>
-          <Grid item xs={2}>
-            <CameraCard />
-          </Grid>
+          {cameras.map((camera, index) => (
+            <Grid item xs={2} key={index}>
+              <CameraCard
+                title={camera.title}
+                estado={camera.estado}
+                onClick={() => handleCardClick(camera.title)}
+              />
+            </Grid>
+          ))}
         </Grid>
-        <Footer />
       </Box>
+      <Footer />
+
+      <Dialog open={isModalOpen}>
+        {/* <DialogTitle>Phone Ringtone</DialogTitle> */}
+        <DialogContent>
+          <StartCamera
+            handleClose={handleClose}
+            cameraTitle={selectedCameraTitle}
+            onStart={handleStart}
+          />
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
