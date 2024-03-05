9c46bbb6998541d87d2105fcd4cb24f371a6787c
Daniel Vargas Castro <104041838+VargasCastroDaniel@users.noreply.github.com>
2024-03-04 23:05:17 -0600

    fix: reafactor

--- src/components/AdminTable.js
+++ src/components/AdminTable.js
@@ -21,6 +21,7 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import AddAdmin from "../modals/AddAdmin";
 import EditAdmin from "../modals/EditAdmin";
+import Header from "./Header";
 
 const AdminTable = () => {
   const [rowsPerPage, setRowsPerPage] = useState(5);
@@ -106,7 +107,6 @@ const AdminTable = () => {
     }
   };
 
-
   const handleClickOpenDelete = (admin) => {
     // logica para eliminar el admin seleccionado
     setOpenDelete(true);
@@ -116,120 +116,123 @@ const AdminTable = () => {
     setOpenDelete(false);
   };
 
-
   return (
-    <div className="container">
-      <h2>Administradores</h2>
-      <div style={{ width: "70rem" }}>
-        <Tooltip title="Agregar" placement="top">
-          <IconButton onClick={handleOpenAdd} aria-label="add">
-            <AddCircleIcon fontSize="large" color="primary" />
-          </IconButton>
-        </Tooltip>
-      </div>
+    <>
+      <Header />
+      <div className="container">
+        <h2>Administradores</h2>
+        <div style={{ width: "70rem" }}>
+          <Tooltip title="Agregar" placement="top">
+            <IconButton onClick={handleOpenAdd} aria-label="add">
+              <AddCircleIcon fontSize="large" color="primary" />
+            </IconButton>
+          </Tooltip>
+        </div>
 
-      <TableContainer sx={{ width: "70rem" }} component={Paper}>
-        <Table sx={{ width: "70rem" }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell align="left">Nombre</TableCell>
-              <TableCell align="left">Apellidos</TableCell>
-              <TableCell align="left">Correo</TableCell>
-              <TableCell align="left">Imagenes</TableCell>
-              <TableCell align="right">Acciones</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {(rowsPerPage > 0
-              ? Admins.slice(
-                  page * rowsPerPage,
-                  page * rowsPerPage + rowsPerPage
-                )
-              : Admins
-            ).map((admin) => (
-              <TableRow
-                key={admin.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {admin.id}
-                </TableCell>
-                <TableCell align="left">{admin.nombre}</TableCell>
-                <TableCell align="left">{admin.apellidos}</TableCell>
-                <TableCell align="left">{admin.correo}</TableCell>
-                <TableCell align="left">{admin.imagenes}</TableCell>
-                <TableCell align="right" sx={{ display: "flex" }}>
-                  <div className="action-btn">
-                    <Tooltip title="Eliminar" placement="top">
-                      <IconButton
-                        color="error"
-                        aria-label="delete"
-                        onClick={() => handleClickOpenDelete(admin)}
-                      >
-                        <DeleteIcon />
-                      </IconButton>
-                    </Tooltip>
-                  </div>
-                  <div className="action-btn">
-                    <Tooltip title="Editar" placement="top">
-                      <IconButton
-                        color="success"
-                        aria-label="edit"
-                        onClick={() => handleOpenEdit(admin)}
-                      >
-                        <EditIcon />
-                      </IconButton>
-                    </Tooltip>
-                  </div>
-                </TableCell>
+        <TableContainer sx={{ width: "70rem" }} component={Paper}>
+          <Table sx={{ width: "70rem" }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell align="left">Nombre</TableCell>
+                <TableCell align="left">Apellidos</TableCell>
+                <TableCell align="left">Correo</TableCell>
+                <TableCell align="left">Imagenes</TableCell>
+                <TableCell align="right">Acciones</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        sx={{ width: "70rem" }}
-        component="div"
-        rowsPerPageOptions={[5, 10, 25]}
-        count={Admins.length}
-        page={page}
-        onPageChange={handleChangePage}
-        rowsPerPage={rowsPerPage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-      <Dialog TransitionProps={{ onEntering: handleEntering }} open={openAdd}>
-        {/* <DialogTitle>Phone Ringtone</DialogTitle> */}
-        <DialogContent>
-          <AddAdmin handleCloseAdd={handleCloseAdd} />
-        </DialogContent>
-      </Dialog>
+            </TableHead>
+            <TableBody>
+              {(rowsPerPage > 0
+                ? Admins.slice(
+                    page * rowsPerPage,
+                    page * rowsPerPage + rowsPerPage
+                  )
+                : Admins
+              ).map((admin) => (
+                <TableRow
+                  key={admin.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {admin.id}
+                  </TableCell>
+                  <TableCell align="left">{admin.nombre}</TableCell>
+                  <TableCell align="left">{admin.apellidos}</TableCell>
+                  <TableCell align="left">{admin.correo}</TableCell>
+                  <TableCell align="left">{admin.imagenes}</TableCell>
+                  <TableCell align="right" sx={{ display: "flex" }}>
+                    <div className="action-btn">
+                      <Tooltip title="Eliminar" placement="top">
+                        <IconButton
+                          color="error"
+                          aria-label="delete"
+                          onClick={() => handleClickOpenDelete(admin)}
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                      </Tooltip>
+                    </div>
+                    <div className="action-btn">
+                      <Tooltip title="Editar" placement="top">
+                        <IconButton
+                          color="success"
+                          aria-label="edit"
+                          onClick={() => handleOpenEdit(admin)}
+                        >
+                          <EditIcon />
+                        </IconButton>
+                      </Tooltip>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          sx={{ width: "70rem" }}
+          component="div"
+          rowsPerPageOptions={[5, 10, 25]}
+          count={Admins.length}
+          page={page}
+          onPageChange={handleChangePage}
+          rowsPerPage={rowsPerPage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+        <Dialog TransitionProps={{ onEntering: handleEntering }} open={openAdd}>
+          {/* <DialogTitle>Phone Ringtone</DialogTitle> */}
+          <DialogContent>
+            <AddAdmin handleCloseAdd={handleCloseAdd} />
+          </DialogContent>
+        </Dialog>
 
-      <Dialog TransitionProps={{ onEntering: handleEntering }} open={openEdit}>
-        <DialogContent>
-          <EditAdmin admin={adminEdit} handleClose={handleCloseEdit} />
-        </DialogContent>
-      </Dialog>
+        <Dialog
+          TransitionProps={{ onEntering: handleEntering }}
+          open={openEdit}
+        >
+          <DialogContent>
+            <EditAdmin admin={adminEdit} handleClose={handleCloseEdit} />
+          </DialogContent>
+        </Dialog>
 
-      <Dialog
-        open={openDelete}
-        onClose={handleCloseDelete}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"¿Está seguro que desea eliminar el admin?"}
-        </DialogTitle>
-        <DialogActions>
-          <Button color="success">
-            Aceptar
-          </Button>
-          <Button color="error" onClick={handleCloseDelete} autoFocus>
-            Cancelar
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+        <Dialog
+          open={openDelete}
+          onClose={handleCloseDelete}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"¿Está seguro que desea eliminar el admin?"}
+          </DialogTitle>
+          <DialogActions>
+            <Button color="success">Aceptar</Button>
+            <Button color="error" onClick={handleCloseDelete} autoFocus>
+              Cancelar
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    </>
   );
 };
 
