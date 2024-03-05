e6a8822272c6aa0aa436411ffb01601d5fac76b8
Daniel Vargas Castro <104041838+VargasCastroDaniel@users.noreply.github.com>
2024-03-04 23:11:17 -0600

    fix: index

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import PageInicio from "./components/PageInicio";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <PageInicio />
+    <App />
   </React.StrictMode>
 );
 
