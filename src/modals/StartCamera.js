e4722fd7aef293ee209c443446323f103a16caa0
Daniel Vargas Castro <104041838+VargasCastroDaniel@users.noreply.github.com>
2024-03-11 17:00:22 -0600

    feat: formlabel y aulas para seleccionarlas en StartCamera.js

--- src/modals/StartCamera.js
+++ src/modals/StartCamera.js
@@ -3,15 +3,34 @@ import {
   Box,
   MenuItem,
   FormControl,
+  FormLabel,
   Typography,
   TextField,
 } from "@mui/material";
+import "../styles/styleStartCamera.css";
 
-function StartCamera({ cameraTitle }) {
+function StartCamera({ cameraTitle, professorName }) {
   const [selectedOption, setSelectedOption] = useState("");
+  const [selectedClassroom, setSelectedClassroom] = useState("");
+
+  const options = [
+    { value: "opcion1", label: "Introducción a la Programación" },
+    { value: "opcion2", label: "QA" },
+    { value: "opcion3", label: "Diseño de Software" },
+  ];
+
+  const classrooms = [
+    { value: "Aula 101", label: "Aula 101" },
+    { value: "Aula 102", label: "Aula 102" },
+    { value: "Aula 103", label: "Aula 103" },
+  ];
 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
+  };
+
+  const handleClassroomChange = (event) => {
+    setSelectedClassroom(event.target.value);
   };
 
   return (
@@ -22,7 +41,12 @@ function StartCamera({ cameraTitle }) {
           <Typography variant="h6" gutterBottom>
             Título de la Cámara: {cameraTitle}
           </Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            Profesor: {professorName}
+          </Typography>
+          <br />
           <FormControl fullWidth>
+            <FormLabel>Selecciona una clase:</FormLabel>
             <TextField
               id="outlined"
               select
@@ -30,14 +54,30 @@ function StartCamera({ cameraTitle }) {
               value={selectedOption}
               onChange={handleOptionChange}
             >
-              <MenuItem value={"opcion1"}>
-                Introducción a la Programación
-              </MenuItem>
-              <MenuItem value={"opcion2"}>QA</MenuItem>
-              <MenuItem value={"opcion3"}>Diseño de Software</MenuItem>
+              {options.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
             </TextField>
           </FormControl>
-          <br />
+          <FormControl fullWidth>
+            <br />
+            <FormLabel>Selecciona un aula disponible:</FormLabel>
+            <TextField
+              id="outlined"
+              select
+              label="Seleccione un aula"
+              value={selectedClassroom}
+              onChange={handleClassroomChange}
+            >
+              {classrooms.map((classroom) => (
+                <MenuItem key={classroom.value} value={classroom.value}>
+                  {classroom.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </FormControl>
         </div>
       </Box>
     </div>
